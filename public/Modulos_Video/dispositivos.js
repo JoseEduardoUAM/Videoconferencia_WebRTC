5f3171abf468042a8eca922bdde3619e30ff1b3c
JoseEduardoUAM <al2172002687@azc.uam.mx>
2021-07-16 16:51:39 -0500

    Cambio ultimo

--- public/Modulos_Video/dispositivos.js
+++ public/Modulos_Video/dispositivos.js
@@ -1,5 +1,5 @@
 import { contenedorVideo , videoLocal } from './identificadores.js';
-import {initConnection} from './conexionwebrtc.js';
+import {iniciarConexion} from './conexionwebrtc.js';
 
 // Abrir la capara para capturar audio y video
 navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
@@ -7,7 +7,7 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream =>
     contenedorVideo.style.display = 'grid';
     videoLocal.srcObject = stream;
     // Inicie una conexión de pares para transmitir la transmisión
-    initConnection(stream);
+    iniciarConexion(stream);
 }).catch(error => console.log(error));
 
 console.log("Entro al modulo de dispositivos");
